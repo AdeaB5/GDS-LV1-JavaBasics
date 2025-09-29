1a63976fdf05374c6707f1f1d9257c9045511d19
AdeaB5 <s56730@edu.campus02.at>
2025-09-29 17:36:01 +0200

    Kommentare hinzugefügt

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -1,7 +1,15 @@
 public class MyFirstClass {
 
     public static void main(String[] args) {
+
+        //Programmstart
+
+        //Zwischen Programmstart und Programmende ist Logik
+        //STRG + Umschalt + Pfeil rauf/runter
+
         System.out.println("My Class");
+
+        // Programmende
 
     }
 }
